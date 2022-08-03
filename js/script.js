a3c0a29050ddebe881798f7fd371b909eba5c2cf
ODILBEK <ooruzmet0v@gmail.com>
2022-08-03 20:23:04 +0500

    firstApp

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
-'use strict'
+"use strict";
 
-console.log('HI');
-nameAngel='aljl'
-console.log(nameAngel);
- var nameAngel;
-
+const numberOfFilms = prompt("How much films you have seen? ", "");
+const movies1 = prompt("One of lastest film from watching films", "");
+const raiting1 = prompt("How hi you raited it?", "");
+const movies2 = prompt("One of lastest film from watching films", "");
+const raiting2 = prompt("How hi you raited it?", "");
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {
+    [movies1]: raiting1,
+    [movies2]: raiting2,
+  },
+  actors: {},
+  genres: [],
+  privat: false,
+};
+console.log(personalMovieDB);
