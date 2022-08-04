a6e08dff2144c8e1997802d80e10b8f8a6e04bbd
ODILBEK <ooruzmet0v@gmail.com>
2022-08-04 23:48:28 +0500

    object

--- js/script.js
+++ js/script.js
@@ -1,49 +1,98 @@
 "use strict";
 
 const personalMovieDB = {
+  start: function () {
+    personalMovieDB.count = prompt(
+      "How much movies you have seen? \n max 9999 ",
+      ""
+    );
+    while (
+      isNaN(+personalMovieDB.count) ||
+      +personalMovieDB.count <= 0 ||
+      personalMovieDB.count.length > 4
+    ) {
+      personalMovieDB.count = prompt(
+        "How much movies you have seen? \n max 9999 ",
+        ""
+      );
+    }
+  },
+  rememberMyFilms: function () {
+    for (let i = 0; i < 2; i++) {
+      let a = prompt(
+        "The last one from movies you have seen . \n max symbol 50!",
+        ""
+      ).trim();
+      while (
+        a === null ||
+        a.length > 50 ||
+        a.length < 2 ||
+        a === "" ||
+        a === 0
+      ) {
+        a = prompt(
+          "The last one from movies you have seen . \n max symbol 50!",
+          ""
+        ).trim();
+      }
+      let b = prompt(
+        "How much you raited it? \n Please enter from 1 to 10",
+        ""
+      );
+      while (isNaN(+b) || +b <= 0 || b > 10) {
+        b = prompt("How much you raited it? \n Please enter from 1 to 10", "");
+      }
+      personalMovieDB.movies[a] = b;
+    }
+  },
+  detectPersonalLevel: function () {
+    if (personalMovieDB.count <= 10) {
+      alert("Less movies you have watchen!");
+    } else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
+      alert("You are classic movie watcher!");
+    } else if (personalMovieDB.count > 30) {
+      alert("You are a kinoman!");
+    } else {
+      alert("Something is wrong!");
+    }
+  },
+  writeYourGenres: function (db) {
+    for (let i = 0; i < 3; i++) {
+      let genre = prompt(`Enter your favorite genre number ${i + 1}`, "");
+      if (genre === null || genre.trim() === "") {
+        i--;
+      } else {
+        // db.genres.push(prompt(`Enter your favorite genre number ${i + 1}`, ""));
+        db.genres[i] = genre.trim();
+      }
+    }
+    personalMovieDB.genres.forEach((genre, i) => {
+      console.log(`Movie '${genre}'  #${i + 1}`);
+    });
+  },
+  showMyDB: function (db) {
+    if (!db.privat) {
+      console.log(personalMovieDB);
+    }
+  },
+  toggleVisibleMyDB: function (db) {
+    if (db.privat) {
+      db.privat = true;
+    } else {
+      db.privat = false;
+    }
+  },
   count: 0,
   movies: {},
   actors: {},
   genres: [],
   privat: false,
 };
-let numberOfFilms;
 
-function start() {
-  numberOfFilms = prompt("How much movies you have seen? \n max 9999 ", "");
-  while (
-    isNaN(+numberOfFilms) ||
-    +numberOfFilms <= 0 ||
-    numberOfFilms.length > 4
-  ) {
-    numberOfFilms = prompt("How much movies you have seen? \n max 9999 ", "");
-  }
-  personalMovieDB.count = +numberOfFilms;
-}
-
-start();
-
-function rememberMyFilms() {
-  for (let i = 0; i < 2; i++) {
-    let a = prompt(
-      "The last one from movies you have seen . \n max symbol 50!",
-      ""
-    );
-    while (a === null || a.length > 50 || a.length < 2 || a === "" || a === 0) {
-      a = prompt(
-        "The last one from movies you have seen . \n max symbol 50!",
-        ""
-      );
-    }
-    let b = prompt("How much you raited it? \n Please enter from 1 to 10", "");
-    while (isNaN(+b) || +b <= 0 || b > 10) {
-      b = prompt("How much you raited it? \n Please enter from 1 to 10", "");
-    }
-    personalMovieDB.movies[a] = b;
-  }
-}
-
-// rememberMyFilms();
+personalMovieDB.start();
+personalMovieDB.rememberMyFilms();
+personalMovieDB.detectPersonalLevel();
+personalMovieDB.writeYourGenres(personalMovieDB);
 
 // for (let i = 0; i < 2; i++) {
 //   const a = prompt(
@@ -66,30 +115,3 @@ function rememberMyFilms() {
 //     i--;
 //   }
 // }
-function detectPersonalLevel() {
-  if (personalMovieDB.count <= 10) {
-    alert("Less movies you have watchen!");
-  } else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
-    alert("You are classic movie watcher!");
-  } else if (personalMovieDB.count > 30) {
-    alert("You are a kinoman!");
-  } else {
-    alert("Something is wrong!");
-  }
-}
-// detectPersonalLevel();
-
-function writeYourGenres(db) {
-  for (let i = 0; i < 3; i++) {
-    // db.genres.push(prompt(`Enter your favorite genre number ${i + 1}`, ""));
-    db.genres[i] = prompt(`Enter your favorite genre number ${i + 1}`, "");
-  }
-}
-writeYourGenres(personalMovieDB);
-
-function showMyDB(db) {
-  if (!db.privat) {
-    console.log(db);
-  }
-}
-showMyDB(personalMovieDB);
--- test.js
+++ test.js
@@ -9,3 +9,57 @@ function calcWithoutReturn(a, b) {
 
 console.log(calcWithoutReturn(5, 6));
 console.log(c);
+
+// callback functions
+
+const callback = function () {
+  setTimeout(() => {
+    console.log("I am callback");
+  }, 2000);
+};
+
+function main(arg, callback) {
+  console.log(arg);
+  callback();
+}
+
+main("I'm argument", callback);
+
+const obj = {
+  name: "Item",
+  itemColor: "red",
+  itemHeight: 100,
+  itemwidth: 300,
+};
+const objToArr = [];
+for (let keys in obj) {
+  objToArr.push(keys);
+}
+console.log(objToArr, objToArr.length);
+console.log(Object.keys(obj), Object.keys(obj).length);
+
+const obj1 = {
+  a: 5,
+  b: 6,
+};
+const copyObj1 = obj1;
+obj1.a = 66;
+console.log(obj1, copyObj1);
+
+const obj2 = {
+  a: 1,
+  b: 3,
+  c: {
+    d: 4,
+    f: 5,
+  },
+};
+const spred = { ...obj2 };
+spred.c.f = 7;
+spred.a = 9;
+const objClone = Object.assign({}, obj2);
+objClone.c.d = 6;
+objClone.a = 6;
+console.log(obj2);
+console.log(objClone);
+console.log(spred);
