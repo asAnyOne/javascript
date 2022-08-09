3a8619fa6f3dd0bfc4dbb20a7f7dabcb2ce45959
ODILBEK <ooruzmet0v@gmail.com>
2022-08-10 00:36:22 +0500

    add timer

--- examplesOnJs/tabs/js/main.js
+++ examplesOnJs/tabs/js/main.js
@@ -1,6 +1,7 @@
 "use strict";
 
 window.addEventListener("DOMContentLoaded", () => {
+  // Tabs--------------
   const tabs = document.querySelectorAll(".tabheader__item"),
     tabContents = document.querySelectorAll(".tabcontent"),
     contentActive = "tabcontent__active",
@@ -24,4 +25,56 @@ window.addEventListener("DOMContentLoaded", () => {
       addActiveClass(tabContents, contentActive, i);
     });
   });
+
+  // timer------------
+
+  const deadLine = "2022-09-20";
+
+  function getTimeRemaining(endtime) {
+    const time = Date.parse(endtime) - Date.parse(new Date()),
+      days = Math.floor(time / (1000 * 60 * 60 * 24)),
+      hours = Math.floor((time / (1000 * 60 * 60)) % 24),
+      minutes = Math.floor((time / (1000 * 60)) % 60),
+      seconds = Math.floor((time / 1000) % 60);
+    return {
+      time,
+      days,
+      hours,
+      minutes,
+      seconds,
+    };
+  }
+  function addZero(num) {
+    if (num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
+  function setTimer(selector, endtime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector("#days"),
+      hours = timer.querySelector("#hours"),
+      minutes = timer.querySelector("#minutes"),
+      seconds = timer.querySelector("#seconds"),
+      timeInterval = setInterval(updateTimer, 1000);
+
+    updateTimer();
+
+    function updateTimer() {
+      const t = getTimeRemaining(endtime);
+
+      days.textContent = addZero(t.days);
+      hours.textContent = addZero(t.hours);
+      minutes.textContent = addZero(t.minutes);
+      seconds.textContent = addZero(t.seconds);
+
+      if (t.time <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  setTimer(".timer", deadLine);
 });
