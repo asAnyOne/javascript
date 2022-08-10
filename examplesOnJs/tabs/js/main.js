d10e1300b06dd321087c320d71ebf0f9ec8252e6
ODILBEK <ooruzmet0v@gmail.com>
2022-08-10 19:13:56 +0500

    modify modal

--- examplesOnJs/tabs/js/main.js
+++ examplesOnJs/tabs/js/main.js
@@ -81,11 +81,13 @@ window.addEventListener("DOMContentLoaded", () => {
   // -------modal------
 
   const modal = document.querySelector(".modal"),
-    btn = document.querySelectorAll("[data-modal]");
+    btn = document.querySelectorAll("[data-modal]"),
+    timerId = setTimeout(toggleModal, 10000);
 
   function toggleModal() {
     modal.classList.toggle("show");
     document.body.classList.toggle("hidden");
+    clearTimeout(timerId);
   }
 
   modal.addEventListener("click", (e) => {
@@ -96,6 +98,20 @@ window.addEventListener("DOMContentLoaded", () => {
       toggleModal();
     }
   });
+
+  function showModalOnPageEnd() {
+    if (
+      document.documentElement.clientHeight +
+        document.documentElement.scrollTop ==
+      document.documentElement.scrollHeight
+    ) {
+      toggleModal();
+      window.removeEventListener("scroll", showModalOnPageEnd);
+    }
+  }
+
+  window.addEventListener("scroll", showModalOnPageEnd);
+
   document.addEventListener("keydown", (e) => {
     if (e.code === "Escape" && modal.classList.contains("show")) {
       toggleModal();
