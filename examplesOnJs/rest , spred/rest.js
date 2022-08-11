288389c04630616385bc59a300cdec81921618f3
ODILBEK <ooruzmet0v@gmail.com>
2022-08-11 15:28:09 +0500

    destructuring , spred, rest

---
+++ examplesOnJs/rest , spred/rest.js
@@ -0,0 +1,25 @@
+"use strict";
+
+function rest(a, b, ...rest) {
+  console.log(a, b, rest);
+}
+
+rest(3, 4, 5, 6, 7); // 3 4 (3)-> [5, 6, 7]
+
+/////////////////////
+
+const arr = [1, 2, 3, 4];
+const obj = { a: 1, b: 2 };
+
+function spred(spred) {
+  const { a, b } = obj;
+  const [c, d, e, f] = arr;
+
+  console.log(...spred);
+
+  console.log(a, b); // 1 2                   destructuring
+  console.log(c, d, e, f); // 1 2 3 4         destructuring
+}
+
+spred(arr); // 1 2 3 4
+// spred(obj); // Uncaught TypeError: Found non-callable @@iterator
