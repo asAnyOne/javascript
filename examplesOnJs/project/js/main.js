5cfd047efcf7a37b62db49e452154df575d2430c
ODILBEK <ooruzmet0v@gmail.com>
2022-08-11 14:10:38 +0500

    class , this ,spred

--- examplesOnJs/project/js/main.js
+++ examplesOnJs/project/js/main.js
@@ -82,7 +82,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
   const modal = document.querySelector(".modal"),
     btn = document.querySelectorAll("[data-modal]"),
-    timerId = setTimeout(toggleModal, 10000);
+    timerId = setTimeout(toggleModal, 100000);
 
   function toggleModal() {
     modal.classList.toggle("show");
@@ -120,4 +120,67 @@ window.addEventListener("DOMContentLoaded", () => {
   btn.forEach((btn) => {
     btn.addEventListener("click", toggleModal);
   });
+
+  //---------class Menu{}
+
+  const menuField = document.querySelector(".menu__field"),
+    menuContainer = menuField.querySelector(".container"),
+    current = 22;
+
+  const itemsData = {
+    data1: [
+      "vegy.jpg",
+      "Фитнес",
+      'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это  абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+      10,
+    ],
+    data2: [
+      "elite.jpg",
+      "Премиум",
+      "   В меню “Премиум” мы используем не только красивый дизайн упаковки,  но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
+      20,
+    ],
+    data3: [
+      "post.jpg",
+      "Постное",
+      "Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля,овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
+      15,
+    ],
+  };
+
+  class MenuCard {
+    constructor(img, title, descr, price, parent) {
+      this.img = img;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.convert(current);
+      this.parent = menuContainer;
+    }
+    convert(curr) {
+      this.price = Math.round(this.price * curr);
+    }
+    render() {
+      const card = document.createElement("div");
+      card.classList.add("menu__item");
+      card.innerHTML = ` 
+      <img src="img/tabs/${this.img} " alt="vegy" />
+      <h3 class="menu__item-subtitle">Меню "${this.title}"</h3>
+      <div class="menu__item-descr">${this.descr} </div> 
+      <div class="menu__item-price">
+        <div class="menu__item-cost">Цена:</div>
+        <div class="menu__item-total">
+          <span>${this.price}
+          </span> 
+        грн/день
+        </div>
+      </div>
+    `;
+      this.parent.append(card);
+    }
+  }
+
+  new MenuCard(...itemsData.data1).render();
+  new MenuCard(...itemsData.data2).render();
+  new MenuCard(...itemsData.data3).render();
 });
