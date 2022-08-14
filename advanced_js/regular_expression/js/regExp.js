bf502183363e9d88a0bad60a4683d80abf3443cc
ODILBEK <ooruzmet0v@gmail.com>
2022-08-14 18:58:56 +0500

    RegExp

---
+++ advanced_js/regular_expression/js/regExp.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// structure for ex.:
+//             1) new RegExp(pattern:string,flags:string)
+//             2)  /pattern/flags
+
+// pattern '.' in js  equal to '*' in css  for ex.:  /./d   all the symbol in the string
+
+//  flags   for ex.:
+//              1) i     registr does'nt matter
+//              2) m     multiline is always on
+//              3) g     all of coincidences
+
+// character classes for ex.:
+//            1) \d     matches any digit (arabic numeral)                               [0-9]          \D   not a digit
+//            2) \w     matches any alphanumeral character from the basic Latin alphabet [A-Za-z0-9]    \W   not a word
+//            3) \s     space                                                                           \S   not a space
+//            4) et cetera
