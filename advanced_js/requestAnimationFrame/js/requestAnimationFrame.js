d7828ceeedb27f50b07598be24c3a6686284d7f9
ODILBEK <ooruzmet0v@gmail.com>
2022-08-18 17:22:28 +0500

    requestAnimationFrame

--- advanced_js/requestAnimationFrame/js/requestAnimationFrame.js
+++ advanced_js/requestAnimationFrame/js/requestAnimationFrame.js
@@ -4,35 +4,37 @@ window.addEventListener("DOMContentLoaded", () => {
     box = document.querySelector(".box");
   let x = 0;
   let side = "left";
-  let bool = false;
+  let bool = true;
 
-  btn.addEventListener("click", () => requestAnimationFrame(down));
+  btn.addEventListener("click", () => requestAnimationFrame(myAnimation));
 
-  function down() {
-    box.style.cssText = `top:${x}px; ${side}:${x}px`;
-    x++;
+  function myAnimation() {
+    if (bool) {
+      box.style.cssText = `top:${x}px; ${side}:${x}px`;
+      x++;
 
-    if (x === 448) {
-      requestAnimationFrame(up);
-    } else if (x < 448) {
-      requestAnimationFrame(down);
-    }
-  }
-
-  function up() {
-    box.style.cssText = `top:${x}px; ${side}:${448}px`;
-    x--;
-
-    if (x === -2) {
-      if (side === "left") {
-        side = "right";
-      } else {
-        side = "left";
+      if (x === 448) {
+        requestAnimationFrame(myAnimation);
+        bool = !bool;
+      } else if (x < 448) {
+        requestAnimationFrame(myAnimation);
       }
-      requestAnimationFrame(down);
-      x = 0;
-    } else if (x > -2) {
-      requestAnimationFrame(up);
+    } else {
+      box.style.cssText = `top:${x}px; ${side}:${448}px`;
+      x--;
+
+      if (x === -2) {
+        if (side === "left") {
+          side = "right";
+        } else {
+          side = "left";
+        }
+        requestAnimationFrame(myAnimation);
+        x = 0;
+        bool = !bool;
+      } else if (x > -2) {
+        requestAnimationFrame(myAnimation);
+      }
     }
   }
 });
