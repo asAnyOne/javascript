65075711d69d07e0b8808d222dd947c34bb72171
ODILBEK <ooruzmet0v@gmail.com>
2022-10-13 19:22:27 +0500

    add bcrypthash secure

--- web-developing/Secrets - Starting Code/app.js
+++ web-developing/Secrets - Starting Code/app.js
@@ -3,8 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 const Schema = mongoose.Schema;
@@ -19,10 +19,6 @@ const userSchema = new Schema({
 	email: String,
 	password: String,
 });
-
-const secret = process.env.SECRET;
-
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -39,33 +35,39 @@ app.get("/register", function (req, res) {
 });
 
 app.post("/register", function (req, res) {
-	const newUser = new User({
-		email: req.body.username,
-		// password: req.body.password,
-		password: md5(req.body.password),
-	});
+	bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+		const newUser = new User({
+			email: req.body.username,
+			password: hash,
+		});
 
-	newUser.save(function (err) {
-		if (err) {
-			console.log(err);
-		} else {
-			res.render("secrets");
-		}
+		newUser.save(function (err) {
+			if (err) {
+				console.log(err);
+			} else {
+				res.render("secrets");
+			}
+		});
 	});
 });
 
 app.post("/login", function (req, res) {
 	const username = req.body.username;
-	// const password = req.body.password;
-	const password = md5(req.body.password);
+	const password = req.body.password;
 
 	User.findOne({ email: username }, function (err, foundUser) {
 		if (err) {
 			console.log(err);
 		} else {
-			if (foundUser && foundUser.password === password) {
-				res.render("secrets");
-			}
+			bcrypt.compare(
+				password,
+				foundUser.password,
+				function (err, result) {
+					if (result) {
+						res.render("secrets");
+					} else console.log(err);
+				}
+			);
 		}
 	});
 });
