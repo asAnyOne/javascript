5bebed3adc55510e8825d76dc87544ba7f11f444
ODILBEK <ooruzmet0v@gmail.com>
2022-10-20 14:59:14 +0500

    adding react-router and route components

--- react-ZTM/ecommerce-clothing-app/src/App.js
+++ react-ZTM/ecommerce-clothing-app/src/App.js
@@ -1,38 +1,16 @@
-import Directory from "./components/directory/directory.component";
+import { Route, Routes } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
+import Home from "./routes/home/home.component";
 
-const App = () => {
-	const categories = [
-		{
-			id: 1,
-			title: "hats",
-			imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-		},
-		{
-			id: 2,
-			title: "jackets",
-			imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-		},
-		{
-			id: 3,
-			title: "sneakers",
-			imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-		},
-		{
-			id: 4,
-			title: "womens",
-			imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-		},
-		{
-			id: 5,
-			title: "mens",
-			imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-		},
-	];
-	return (
-		<div className="App">
-			<Directory categories={categories} />
-		</div>
-	);
-};
+const Shop = () => <h1>The shop page</h1>;
+
+const App = () => (
+	<Routes>
+		<Route path="/" element={<Navigation />}>
+			<Route index element={<Home />} />;
+			<Route path="shop" element={<Shop />} />
+		</Route>
+	</Routes>
+);
 
 export default App;
---
+++ react-ZTM/ecommerce-clothing-app/src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,27 @@
+import { Fragment } from "react";
+import { Link, Outlet } from "react-router-dom";
+
+import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
+import "./navigation.styles.scss";
+
+const Navigation = () => {
+	return (
+		<Fragment>
+			<div className="navigation">
+				<div className="logo-container">
+					<Link to="/">
+						<CrownLogo className="logo" />
+					</Link>
+				</div>
+				<div className="nav-links-container">
+					<Link className="nav-link" to="/shop">
+						Shop
+					</Link>
+				</div>
+			</div>
+			<Outlet />
+		</Fragment>
+	);
+};
+
+export default Navigation;
