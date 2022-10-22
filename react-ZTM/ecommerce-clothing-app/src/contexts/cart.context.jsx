bcc813d39af0b96fda05d50e06d7f156f6145e69
ODILBEK <ooruzmet0v@gmail.com>
2022-10-22 17:29:08 +0500

    added in our addToCart feature

--- react-ZTM/ecommerce-clothing-app/src/contexts/cart.context.jsx
+++ react-ZTM/ecommerce-clothing-app/src/contexts/cart.context.jsx
@@ -1,13 +1,51 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+
+const addCartItem = (cartItems, productAdd) => {
+	const existingCartItem = cartItems.find(
+		(cartItem) => cartItem.id === productAdd.id
+	);
+	if (existingCartItem) {
+		return cartItems.map((cartItem) => {
+			return cartItem.id === productAdd.id
+				? { ...cartItem, quantity: cartItem.quantity + 1 }
+				: cartItem;
+		});
+	} else {
+		return [...cartItems, { ...productAdd, quantity: 1 }];
+	}
+};
 
 export const CartContext = createContext({
 	isCartOpen: false,
 	setIsCartOpen: () => {},
+	cartItems: [],
+	addItemToCart: () => {},
+	cartCount: 0,
 });
 
 export const CartProvider = ({ children }) => {
 	const [isCartOpen, setIsCartOpen] = useState(false);
-	const value = { isCartOpen, setIsCartOpen };
+	const [cartItems, setCartItems] = useState([]);
+	const [cartCount, setCartCount] = useState(0);
+
+	useEffect(() => {
+		const newCartCount = cartItems.reduce(
+			(total, cartItem) => total + cartItem.quantity,
+			0
+		);
+		setCartCount(newCartCount);
+	}, [cartItems]);
+
+	const addItemToCart = (productAdd) => {
+		setCartItems(addCartItem(cartItems, productAdd));
+	};
+	const value = {
+		isCartOpen,
+		setIsCartOpen,
+		addItemToCart,
+		cartItems,
+		cartCount,
+	};
 
 	return (
 		<CartContext.Provider value={value}>{children}</CartContext.Provider>
